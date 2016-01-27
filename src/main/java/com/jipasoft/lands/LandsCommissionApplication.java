78d84d0fae63444fac0642ca0abaf0f03d8743a7
Julius Krah <juliuskrah@gmail.com>
2016-01-27 22:17:31 +0000

    init repo

---
+++ src/main/java/com/jipasoft/lands/LandsCommissionApplication.java
@@ -0,0 +1,12 @@
+package com.jipasoft.lands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandsCommissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandsCommissionApplication.class, args);
+	}
+}
